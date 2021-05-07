33e5f48ba06f1c2787d9e5186c2c570bba70d5da
Hue Phuong Le s3687477 <s3687477@student.rmit.edu.au>
2021-05-08 01:22:31 +0700

    setup frontend

---
+++ Frontend/my-app/src/App.js
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router, Switch} from "react-router-dom";
+
+function App()
+{
+  return(
+      <Router>
+        <Switch>
+
+        </Switch>
+      </Router>
+  );
+}
+
+export default App;
